e01b78c854dbe76d4963be48daf77db0a7aa403d
GLinBoy <hojjatabedie@gmail.com>
2022-09-01 23:47:46 +0200

    👌 IMPROVE: Added methods to get bill(s) to BillServiceApi

--- src/main/kotlin/com/glinboy/biller/service/BillServiceApi.kt
+++ src/main/kotlin/com/glinboy/biller/service/BillServiceApi.kt
@@ -1,4 +1,11 @@
 package com.glinboy.biller.service
 
+import com.glinboy.biller.entity.Bill
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import java.util.*
+
 interface BillServiceApi {
+    fun getBills(pageable: Pageable): Page<Bill>
+    fun getBill(id: String): Optional<Bill>
 }
