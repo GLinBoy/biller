83a3f351148b27e3c1ef392e97c91831f48db8d4
GLinBoy <hojjatabedie@gmail.com>
2022-09-02 21:41:38 +0200

    👌 IMPROVE: Added BillResource class

---
+++ src/main/kotlin/com/glinboy/biller/web/rest/BillResource.kt
@@ -0,0 +1,19 @@
+package com.glinboy.biller.web.rest
+
+import com.glinboy.biller.entity.Bill
+import com.glinboy.biller.service.BillServiceApi
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
+import java.util.*
+
+@RestController
+@RequestMapping("/bills")
+class BillResource(private val billService: BillServiceApi) {
+}
