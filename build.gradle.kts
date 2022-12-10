abcbf81f79a9b82b6df1bb21ee51cb28cfdabcdb
GLinBoy <hojjatabedie@gmail.com>
2022-12-10 13:33:09 +0100

    👌 IMPROVE: Upgraded springdoc-openapi-ui [1.6.11 -> 1.6.13]
    #64

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.13")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
