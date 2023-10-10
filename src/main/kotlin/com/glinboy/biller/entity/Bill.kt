6732a4be54d5f02ee23cfa8a05edbe804f37ed6b
GLinBoy <hojjatabedie@gmail.com>
2023-10-10 20:20:48 +0200

    👌 IMPROVE: Added createdAt field to Bill

--- src/main/kotlin/com/glinboy/biller/entity/Bill.kt
+++ src/main/kotlin/com/glinboy/biller/entity/Bill.kt
@@ -2,6 +2,10 @@ package com.glinboy.biller.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDateTime
 
 @Document
-data class Bill(@Id val id: String)
+data class Bill(
+    @Id val id: String?,
+    val createdAt: LocalDateTime?
+)
