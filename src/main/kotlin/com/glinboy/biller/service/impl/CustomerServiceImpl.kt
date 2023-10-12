42e0162c891bd65cc97b5b98068ef08e0bbe5279
GLinBoy <hojjatabedie@gmail.com>
2023-10-12 20:24:23 +0200

    👌 IMPROVE: Injected CustomerRepository to its service

--- src/main/kotlin/com/glinboy/biller/service/impl/CustomerServiceImpl.kt
+++ src/main/kotlin/com/glinboy/biller/service/impl/CustomerServiceImpl.kt
@@ -1,6 +1,7 @@
 package com.glinboy.biller.service.impl
 
+import com.glinboy.biller.repository.CustomerRepository
 import com.glinboy.biller.service.CustomerServiceApi
 
-class CustomerServiceImpl : CustomerServiceApi {
+class CustomerServiceImpl(private val customerRepository: CustomerRepository) : CustomerServiceApi {
 }
