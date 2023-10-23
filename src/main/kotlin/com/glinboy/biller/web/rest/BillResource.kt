edc9c1e8db41c4f3ca951f21fe4bea39c17f3e7b
GLinBoy <hojjatabedie@gmail.com>
2023-10-23 20:02:12 +0200

    👌 IMPROVE: Exposed create Bill

--- src/main/kotlin/com/glinboy/biller/web/rest/BillResource.kt
+++ src/main/kotlin/com/glinboy/biller/web/rest/BillResource.kt
@@ -8,10 +8,12 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
-import java.util.*
+import java.net.URI
 
 @RestController
 @RequestMapping("/bills")
@@ -25,4 +27,12 @@ class BillResource(private val billService: BillServiceApi) {
         .map { ResponseEntity.ok(it)}
         .orElseThrow { ResponseStatusException(HttpStatus.NOT_FOUND,
             "Couldn't find bit with id $id") }
+
+    @PostMapping
+    fun createBill(@RequestBody bill: Bill): ResponseEntity<Bill> {
+        val savedBill: Bill = billService.saveBill(bill)
+        return ResponseEntity
+            .created(URI("/bills/${savedBill.id}"))
+            .body(savedBill)
+    }
 }
