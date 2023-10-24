698b77c597a77967dff50ec18e898987706ce750
GLinBoy <hojjatabedie@gmail.com>
2023-10-24 19:34:23 +0200

    👌 IMPROVE: Enhanced fields of Bill

--- src/main/kotlin/com/glinboy/biller/entity/Bill.kt
+++ src/main/kotlin/com/glinboy/biller/entity/Bill.kt
@@ -2,11 +2,15 @@ package com.glinboy.biller.entity
 
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 @Document
 data class Bill(
     @Id val id: String?,
+    val invoiceNumber: Long?,
+    val invoiceDate: LocalDate?,
+    val items: Set<Item>?,
+    val customer: Customer?,
     val createdAt: LocalDateTime?,
-    val items: Set<Item>?
 )
