d1f09e402e872c08bba7e8bd6b5d42433458a301
GLinBoy <hojjatabedie@gmail.com>
2023-10-24 19:34:16 +0200

    👌 IMPROVE: Enhanced fields of Customer

--- src/main/kotlin/com/glinboy/biller/entity/Customer.kt
+++ src/main/kotlin/com/glinboy/biller/entity/Customer.kt
@@ -1,5 +1,9 @@
 package com.glinboy.biller.entity
 
 data class Customer(
-    val id: String?
+    val id: String?,
+    val firstname: String?,
+    val lastname: String?,
+    val address: String?,
+    val phone: String?,
 )
