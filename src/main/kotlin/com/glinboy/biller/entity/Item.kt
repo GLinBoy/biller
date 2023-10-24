ba29b6f738c7bb8dc9bb9081235ce5e8277901cf
GLinBoy <hojjatabedie@gmail.com>
2023-10-24 19:34:06 +0200

    👌 IMPROVE: Enhanced fields of Item

--- src/main/kotlin/com/glinboy/biller/entity/Item.kt
+++ src/main/kotlin/com/glinboy/biller/entity/Item.kt
@@ -1,5 +1,11 @@
 package com.glinboy.biller.entity
 
+import java.math.BigDecimal
+
 data class Item(
-    val id: String?
+    val id: String?,
+    val no: Int?,
+    val description: String?,
+    val quantity: Int?,
+    val rate: BigDecimal?,
 )
