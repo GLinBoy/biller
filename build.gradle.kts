608a1f69580ff3317c5c0d927d9f8136c8e9a90d
GLinBoy <hojjatabedie@gmail.com>
2023-12-17 12:55:54 +0100

    👌 IMPROVE: Removed unnecessary dependency
    #118

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$openapiVersion")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
