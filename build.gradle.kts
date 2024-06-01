b7dd063902f0d730ca414a7fd2908d9aa526aaf7
GLinBoy <glinboy@live.com>
2024-06-01 15:26:46 +0200

    👌 IMPROVE: Upgraded dependencies
    resolved #144

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,8 +17,8 @@ repositories {
 	mavenCentral()
 }
 
-val openapiVersion = "2.3.0"
-val embedMongoVersion = "4.12.2"
+val openapiVersion = "2.5.0"
+val embedMongoVersion = "4.13.1"
 
 extra["testcontainersVersion"] = "1.19.5"
 
